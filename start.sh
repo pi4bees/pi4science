b916ea653493c5515ae19a97928722366dba5831
pi4bees <hns36@cornell.edu>
2017-06-18 17:29:42 -0400

    updating data

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d "/media/pi/EMTEC" ]; then
-	sudo umound /media/pi/EMTEC
-fi
-
-MOUNT_LOCATION=$(sudo blkid | grep /dev/s | cut -c1-9)
-#sudo chown pi:pi -R pi
-sudo mount $MOUNT_LOCATION /media/pi/ -o umask=000,uid=pi,gid=pi
